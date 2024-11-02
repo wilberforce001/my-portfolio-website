8af430c3f9a456d7ce842e2755048a133de4443f
wilberforce001 <kenmisik@gmail.com>
2024-11-02 11:41:15 +0300

    Home and Navbar styling

--- kipyegonwilber/src/components/HomePage.js
+++ kipyegonwilber/src/components/HomePage.js
@@ -4,18 +4,22 @@ import cursorIcon from '../assets/cursorIcon.svg';
 
 const HomePage = () => {
     return (
-        <section className="home d-flex flex-column justify-content-center align-items-center text-center bg-light vh-100">
+        <section className="home d-flex flex-column justify-content-center align-items-center text-center vh-100">
             <div className="container">
-                <h1 className="lead text-secondary mb-3">
-                Dedicated to Crafting Engaging and Functional User Interfaces                </h1>
-                <p className="display-4 fw-bold text-dark mb-3">
-                    Transforming Concepts into Seamless User Experiences
+                <h1 className="fs-8 lead text-secondary mb-5">
+                DEDICATED TO CRAFTING ENGAGING AND FUNCTIONAL USER INTERFACES
+                </h1>
+                <p className="fs-24 display-4 fw-bold text-white mb-5">
+                    Transforming Concepts into <br />
+                    Seamless User Experiences
                 </p>
-                <p className="fs-5 text-muted">
-                    Hi! 👋🏾 I'm Wilberforce, a Fullstack Developer with a background in Actuarial Science. 
-                    I love building elegant, user-friendly web experiences that bring ideas to life.
+                <p className="fs-5 text-secondary mb-5">
+                    Hi! 👋🏾 I'm Wilberforce, a Fullstack Developer
+                    blending tech with a touch of Actuarial precision <br />
+                    to create sleek, user-focused web experiences.
                 </p>
-                <div className="d-flex justify-content-center align-items-center">
+
+                <div className="button-container d-flex justify-content-center align-items-center">
                 <Button variant="primary" className="d-flex align-items-center">
                     View My Work
                     <img src={cursorIcon} alt="Cursor Icon" style={{ marginLeft: '15px' }} width="20" height="20" />
--- kipyegonwilber/src/components/Navbar.js
+++ kipyegonwilber/src/components/Navbar.js
@@ -1,21 +1,39 @@
-import React from 'react';
+// NavigationBar.js
+import React, { useState, useEffect } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import '../styles/styles.css';
 
-const NavigationBar = () => (
-    <Navbar className="navbar-custom" expand="lg">
-    <Navbar.Brand href="#home"></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-      <Nav className="ml-auto text-center">
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#skills">Skills</Nav.Link>
-        <Nav.Link href="#projects">Projects</Nav.Link>
-        <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
+const NavigationBar = () => {
+  const [navbarBg, setNavbarBg] = useState(false);
+
+  const changeNavbarBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbarBg(true);
+    } else {
+      setNavbarBg(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavbarBackground);
+    return () => window.removeEventListener('scroll', changeNavbarBackground);
+  }, []);
+
+  return (
+    <Navbar
+      expand="lg"
+      className={`navbar-custom ${navbarBg ? 'navbar-bg-dark fixed-top' : 'navbar-center'}`}
+    >
+      <Navbar.Brand href="#home" className="text-light"></Navbar.Brand>
+      <Nav className="ml-auto flex-row text-center">
+        <Nav.Link href="#about" className="text-light">About</Nav.Link>
+        <Nav.Link href="#skills" className="text-light">Skills</Nav.Link>
+        <Nav.Link href="#projects" className="text-light">Projects</Nav.Link>
+        <Nav.Link href="#testimonials" className="text-light">Testimonials</Nav.Link>
+        <Nav.Link href="#contact" className="text-light">Contact</Nav.Link>
       </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+    </Navbar>
+  );
+};
 
 export default NavigationBar;
