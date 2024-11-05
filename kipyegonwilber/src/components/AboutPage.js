08d3dc29c397eef7b7f47d3a53a686c3f9a23bf5
wilberforce001 <kenmisik@gmail.com>
2024-11-05 11:16:23 +0300

    Professional and education journey

--- kipyegonwilber/src/components/AboutPage.js
+++ kipyegonwilber/src/components/AboutPage.js
@@ -8,40 +8,59 @@ const AboutPage = () => {
     const educationContent = (
         <div className="educationContent">
           <h2>My Education Journey</h2>
-          <p>With a degree in Disaster Preparedness and Environmental Technology from Masinde Muliro University <br />
-             and mobile development training at Moringa School, my education has been a blend of technical expertise <br />
-             and problem-solving. Here’s a timeline of the key milestones that have shaped my journey as a multidisciplinary developer.</p>
+          <p className="journey-text">My educational journey has been rooted in a strong foundation in Actuarial Science, 
+            which has equipped me with analytical and quantitative skills. Throughout my studies,
+             I developed a deep understanding of mathematical principles, statistics, and risk assessment. 
+             This academic background has not only honed my problem-solving abilities but also sparked my interest in software engineering, 
+            leading me to pursue further learning and practical experience in this dynamic field.</p>
             <ul className="professional-journey">
             <li>
               <div className="professional-title">
-                <h2>Software Developer</h2>
+                <h2>Kenyatta University</h2>
               </div>
               <div className="skills-year-container">
                 <div className="skill-tags">
-                  <span className="skill-tag">React</span>
-                  <span className="skill-tag">JavaScript</span>
-                  <span className="skill-tag">Vue.js</span>
-                  <span className="skill-tag">Node.js</span>
-                  <span className="skill-tag">Express</span>
+                  <span className="skill-tag">B.Sc. (Actuarial Science)</span>
                 </div>
-                <span className="year">2022</span>
+                <span className="year">July, 2021</span>
               </div>
-              <div className="description-container">
-                <p className="description">Acquired in-depth knowledge of Java, focusing on building efficient, high-performance Android applications.</p>
-                <p className="description">Learned to use the Android SDK for app development and mastered Android-specific UI components to create responsive and user-friendly interfaces.</p>
-                <p className="description">Gained experience with data storage techniques in Android, including SQLite databases, SharedPreferences, and content providers.</p>
-                <p className="description">Developed a strong understanding of the Android app lifecycle and learned best practices for optimizing app performance and memory usage.</p>
+              <div className="professional-title">
+                <h2>European Business University of Luxembourg</h2>
+              </div>
+              <div className="skills-year-container">
+                <div className="skill-tags">
+                  <span className="skill-tag">Certificate in Mobile Application Development</span>
+                </div>
+                <span className="year">Sep. 2022 - May 2023</span>
+              </div>
+              <div className="professional-title">
+                <h2>European Business University of Luxembourg</h2>
+              </div>
+              <div className="skills-year-container">
+                <div className="skill-tags">
+                  <span className="skill-tag">Certificate in Introduction to Python </span>
+                </div>
+                <span className="year">Jult 2022 - Oct. 2022</span>
+              </div>
+              <div className="professional-title">
+                <h2>European Business University of Luxembourg</h2>
+              </div>
+              <div className="skills-year-container">
+                <div className="skill-tags">
+                  <span className="skill-tag">Certificate in Financial Accounting</span>
+                </div>
+                <span className="year">March 2022 - June 2022</span>
               </div>
             </li>
           </ul>
         </div>
       );
       const experienceContent = (
-        <div>
+        <div className="prof-journey">
           <h2>My Professional Journey</h2>
-          <p>With extensive experience in both full-time and consultancy roles, <br />
-          I specialize in frontend development and user-centric design. Here's a timeline of <br />
-           my journey and the projects I've contributed to along the way.</p>
+          <p className="journey-text">
+          Over the past two years, I have transitioned into software engineering, leveraging my analytical background in Actuarial Science. My journey has been characterized by self-directed learning and hands-on experience through an intensive two-week bootcamp, where I enhanced my skills in front-end and back-end development. Although I haven't yet worked in an organization, I've built a portfolio that showcases my ability to create functional UIs, manage authentication, and implement CRUD functionality. I am eager to find opportunities that align with my values and allow me to grow as a software engineer.
+          </p>
           <ul className="professional-journey">
             <li>
               <div className="professional-title">
@@ -49,8 +68,8 @@ const AboutPage = () => {
               </div>
               <div className="skills-year-container">
                 <div className="skill-tags">
-                  <span className="skill-tag">React</span>
                   <span className="skill-tag">JavaScript</span>
+                  <span className="skill-tag">React</span>
                   <span className="skill-tag">Vue.js</span>
                   <span className="skill-tag">Node.js</span>
                   <span className="skill-tag">Express</span>
@@ -58,10 +77,9 @@ const AboutPage = () => {
                 <span className="year">2022</span>
               </div>
               <div className="description-container">
-                <p className="description">Acquired in-depth knowledge of Java, focusing on building efficient, high-performance Android applications.</p>
-                <p className="description">Learned to use the Android SDK for app development and mastered Android-specific UI components to create responsive and user-friendly interfaces.</p>
-                <p className="description">Gained experience with data storage techniques in Android, including SQLite databases, SharedPreferences, and content providers.</p>
-                <p className="description">Developed a strong understanding of the Android app lifecycle and learned best practices for optimizing app performance and memory usage.</p>
+                <p className="description">Acquired in-depth knowledge of web development, focusing on building structured and responsive applications using HTML, CSS, and JavaScript. Developed proficiency in server-side programming with Node.js and Express, enabling the creation of scalable RESTful APIs.</p>
+                <p className="description">Gained experience in modern front-end frameworks such as React and Vue.js, emphasizing component-based architecture and dynamic user interfaces. Enhanced code quality through the use of TypeScript, and mastered database management with both relational (Postgres, MySQL) and NoSQL (MongoDB) databases.</p>
+                <p className="description">Applied modern CSS frameworks like Tailwind CSS and Bootstrap to create visually appealing and responsive designs, ensuring a seamless user experience.</p>
               </div>
             </li>
           </ul>
