c63d48f18bd017c77accd006630d98a87c6d1eb2
wilberforce001 <kenmisik@gmail.com>
2024-11-08 20:31:20 +0300

    Fine tuned homepage

--- kipyegonwilber/src/components/HomePage.js
+++ kipyegonwilber/src/components/HomePage.js
@@ -4,19 +4,16 @@ import cursorIcon from '../assets/cursorIcon.svg';
 
 const HomePage = () => {
     return (
-        <section className="home d-flex flex-column justify-content-center align-items-center text-center vh-100">
+        <section className="home d-flex flex-column justify-content-center align-items-center text-center">
             <div className="container">
-                <h1 className="">
-                {/* fs-8 lead text-secondary mb-5 */}
+                <h1 className="fs-8 lead text-secondary mb-5">
                 DEDICATED TO CRAFTING ENGAGING AND FUNCTIONAL USER INTERFACES
                 </h1>
-                <p>
-                {/* fs- 24 display-4 fw-bold text-red mb-5 */}
+                <p className="fs-24 display-4 fw-bold text-red mb-5">
                     Transforming Concepts into <br />
                     Seamless User Experiences
                 </p>
-                <p className="">
-                    {/* mfs-5 text-secondary mb-5 */}
+                <p className="fs-5 text-secondary mb-5">
                     Hi!👋🏾 I'm Wilberforce, a Fullstack Developer
                     blending tech with a touch of Actuarial precision <br />
                     to create sleek, user-focused web experiences.
